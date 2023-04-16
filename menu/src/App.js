ca1699179931af7bb454d4c29187b2bacaa47df2
goosefin <agustinafin96@gmail.com>
2023-04-16 18:28:16 -0400

    iterating over seeded data in the Menu component

--- menu/src/App.js
+++ menu/src/App.js
@@ -4,13 +4,23 @@ import Categories from './Categories'
 import items from './data'
 
 function App() {
-  return (
-    <>
-      <h2>Menu Project Setup</h2>
-      <img src="./images/item-8.jpeg" />
-    </>
-    
-  );
+  //initializing the menuItems with start value of the array in data.js
+  const [menuItems,setMenuItems] = useState(items)
+  const [categories, setCategories] = useState([])
+
+  return(
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+
+        <Categories/>
+        <Menu items={menuItems}/>
+      </section>
+    </main>
+  )
 }
 
 export default App;
